549256bcb6f4b39c4a052ba3aad8c263c34e9dc7
caiogalassi <caiogalaxi71@gmail.com>
2025-05-16 17:08:32 +0000

    resolucao exercicios lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,20 @@
 # Crie uma lista apenas com elementos numéricos
+lista = [1,2,3,4,5,6, 7, 8, 9, 10]
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+listaMista = ["Python" , 10, [1,2,3,4], True]
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
+print(lista[0:5])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print(lista[0:-1:2])
 # Remova da lista o último item
-
+lista.remove(10)
+print(lista)
 # Insira na lista um novo item
-
+lista.append(10)
+print(lista)
 # Remova da lista um item específico
+lista.remove(1)
+print(lista)
