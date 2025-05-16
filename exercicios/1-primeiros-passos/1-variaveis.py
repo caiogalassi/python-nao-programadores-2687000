b27bb9f18d17c07530320214c80981946c23c138
caiogalassi <caiogalaxi71@gmail.com>
2025-05-16 14:57:51 +0000

    resolucao exercicio variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'caio'
 # Declare variáveis de cada tipo numérico
-
+numeroInteiro = 9
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
+
+# testando as variáveis
+print (nome)
+print (numeroInteiro)
+print (verdadeiro)
+print (falso)
