2e440bacd412c1dc9ee22d8cda573a1bee8d5553
caiogalassi <caiogalaxi71@gmail.com>
2025-05-16 16:39:46 +0000

    resolucao exercicio numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,19 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+frase = "Hoje eu estudei muito \n"
+print(frase * 3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+
+num1 = 23
+num2 = 0.3
+
+soma = num1 + num2
+
+print (soma)
